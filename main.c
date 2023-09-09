dd36f4fe60ebcc098613f29b32699f798195cce1
Justin Federico <50191132+justinfederico@users.noreply.github.com>
2023-09-09 16:42:53 -0700

    Update main.c
    Messed around with the MUX a bit

--- main.c
+++ main.c
@@ -1,1 +1,38 @@
+#include <Wire.h>
+volatile bool interruptOccurred = false;
+void handleInterrupt() {
+  interruptOccurred = true;
+}
+// Select I2C BUS
+void TCA9548A(uint8_t bus){
+ 
+  Wire.beginTransmission(0x70);  // TCA9548A address
+  Wire.write(1 << bus);          // send byte to select bus
+  Wire.endTransmission();
+  Serial.print(bus);
+}
+void setup() {
+  // Set up serial communication
+//   Serial.begin(9600);
+//   Wire.begin();
+//   TCA9548A(2);
+//   if (!joint1Encoder.begin(0x76)) {
+//     Serial.println("Could not find a valid device on bus 2, check your wiring!");
+//     while (1);
+//   }
+//   Serial.println();
+  
+//   TCA9548A(3);
+//   if (!joint2Encoder.begin(0x76)) {
+//     Serial.println("Could not find a valid device on bus 3, check your wiring!!");
+//     while (1);
+//   }
+//   Serial.println();
+// }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
 
