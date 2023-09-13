371b6f4934d517b66045df75ad5dc44392442255
Justin Federico <jfederico11@gmail.com>
2023-09-12 21:04:30 -0700

    screen worked, added skeleton MPPT stuff

--- SolarArduino/src/main.cpp
+++ SolarArduino/src/main.cpp
@@ -3,6 +3,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+//Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 //Measurement Variables
 const float AVCC = 4.863;
 float panelAmps;
@@ -13,6 +16,8 @@ float currentSolarWatts;
 float previousSolarWatts;
 unsigned int interruptCounter;
 boolean load = false; //assume initial state of uncharged
+boolean daytime = true; //make sure system knows if sleep should be turned off
+
 
 //screen constants rec by adafruit
 #define TRUE 1
@@ -64,9 +69,68 @@ void TCA9548A(uint8_t bus){
   Wire.endTransmission();
   Serial.print(bus);
 }
+float readSolarPanelVoltage() {
+  // Read voltage using voltage divider (prolly two resistors)
+  // int rawValue = analogRead(voltagePin);
+  // float voltage = (rawValue * (float)analogRead(A0)) / 1024.0;
+  // voltage = voltage / 2.0; // Adjust for voltage divider
+  // return voltage;
+}
 
-// 
+float readCurrent() {
+  // Read current using hall effect IC
+  // int rawValue = analogRead(currentSensorPin);
+  // float current = (rawValue * (float)analogRead(A0)) / 1024.0;
+  // current = current / (0.1); // Adjust for sensor sensitivity
+  // return current;
+}
+// MPPT Algorithm function
+void mpptAlgorithm() {
+  // Variables specific to the MPPT algorithm
+  float maxPower = 0.0; // Maximum power achieved so far
+  float optimalVoltage = 0.0; // Optimal voltage for maximum power
+  float currentVoltage = 0.0; // Current measured solar panel voltage
+  float currentCurrent = 0.0; // Current measured current from the sensor
+
+  // Read solar panel voltage
+  currentVoltage = readSolarPanelVoltage();
+
+  // Read current using Hall effect sensor
+  currentCurrent = readCurrent();
+
+  // Implement MPPT algorithm here
+  float power = currentVoltage * currentCurrent;
+  if (power > maxPower) {
+    maxPower = power;
+    optimalVoltage = currentVoltage;
+    // i assume this is where the load adjustment stuff with mosfets and switches comes in
+  }
+
+  // Display data on OLED
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Solar Panel MPPT");
+  display.setTextSize(2);
+  display.setCursor(0, 20);
+  display.print("Voltage: ");
+  display.print(currentVoltage);
+  display.setCursor(0, 40);
+  display.print("Current: ");
+  display.print(currentCurrent);
+  display.display();
+}
+
+
+
 void setup() {
+  // Initialize OLED display
+  int dummy; //get rid of this, need actual address
+  if (!display.begin(dummy, OLED_RESET)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
   Serial.begin(115200);
   //Set up serial communication
   // Serial.begin(9600);
